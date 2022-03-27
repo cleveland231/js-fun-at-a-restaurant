c924d7efcb0dd69d19f9a59aaadfc355bdf43759
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-03-27 17:52:08 -0600

    Add meal file and test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
 }
+function createMenuItem(item, amount, time) {
+  return {
+    name: item,
+    price: amount,
+    type: time,
+  };
+}
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.includes(newIngredient)) {
+    return ingredients
+  } else {
+  return ingredients.push(newIngredient);
+  }
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
+
+// function decreasePrice(initialPrice) {
+//   var product = (initialPrice * .10)
+//   return initialPrice - product;
+// };
+
+function decreasePrice(initialPrice) {
+  var product = (initialPrice * .9)
+  return product
+};
+
+function createRecipe(titleS, ingredientS, typeS) {
+  return {
+    title: titleS,
+    ingredients: ingredientS,
+    type: typeS
+  }
+}
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
